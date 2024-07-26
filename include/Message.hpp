336e73cb4575a6369fc549864f394d9771ad6e7d
aurlic <arthur.urlic@gmail.com>
2024-07-26 15:41:05 +0200

    fix: formatting

--- include/Message.hpp
+++ include/Message.hpp
@@ -4,33 +4,37 @@
 #include <vector>
 
 class Message {
-	protected:
-		static const unsigned int	err_no_cmd = 1001;
-		static const unsigned int	err_syntax = 1002;
-		static const unsigned int	max_params = 15;
+  protected:
+    static const unsigned int max_params = 15;
 
-		std::string					_prefix;
-		std::string					_command;
-		std::vector<std::string> 	_params;
+    std::string _prefix;
+    std::string _command;
+    std::vector<std::string> _params;
 
-		static int parse(std::string& data,std::string& prefix, std::string& command, std::vector<std::string>& params);
-		static int validate(const std::string& prefix, const std::string& command, const std::vector<std::string>& params);
+    static bool parse(std::string& data, std::string& prefix,
+                      std::string& command, std::vector<std::string>& params);
+    static bool validate(const std::string& prefix, const std::string& command,
+                         const std::vector<std::string>& params);
 
-	public:
-		virtual ~Message();
+  public:
+    virtual ~Message();
 
-		std::string getRawMsg();
+    std::string getRawMsg();
 
-		static void create(std::string& data);
+    static void create(std::string& data);
 
-		virtual void run() = 0;
+    virtual void run() = 0;
 
-		class MissingCommandException : public std::exception {
-			public:
-				virtual const char* what() const throw() {return ("Error: Missing command in message.");}
-		};
-		class InvalidFormatException : public std::exception {
-			public:
-				virtual const char* what() const throw() {return ("Error: Message format is invalid.");}
-		};
+    class MissingCommandException : public std::exception {
+      public:
+        virtual const char* what() const throw() {
+            return ("Error: Missing command in message.");
+        }
+    };
+    class InvalidFormatException : public std::exception {
+      public:
+        virtual const char* what() const throw() {
+            return ("Error: Message format is invalid.");
+        }
+    };
 };
--- src/Message.cpp
+++ src/Message.cpp
@@ -4,82 +4,86 @@
 
 Message::~Message() {}
 
-void	Message::create(std::string& data) {
-	std::string					prefix, command;
-	std::vector<std::string>	params;
+void Message::create(std::string& data) {
+    std::string prefix, command;
+    std::vector<std::string> params;
 
-	if (Message::parse(data, prefix, command, params) == err_no_cmd) {
-		throw (Message::MissingCommandException());
-	}
-	if (Message::validate(prefix, command, params) == err_syntax) {
-		throw (Message::InvalidFormatException());
-	}
+    if (Message::parse(data, prefix, command, params) == false) {
+        throw(Message::MissingCommandException());
+    }
+    if (Message::validate(prefix, command, params) == false) {
+        throw(Message::InvalidFormatException());
+    }
 
-	// ici message valide donc appel de commande etc
+    // ici message valide donc appel de commande etc
 }
 
-int Message::parse(std::string& data, std::string& prefix, std::string& command, std::vector<std::string>& params) {
-	std::istringstream			iss(data);
-	std::string					word;
+bool Message::parse(std::string& data, std::string& prefix,
+                    std::string& command, std::vector<std::string>& params) {
+    std::istringstream iss(data);
+    std::string word;
 
-	if (data[0] == ':') {
-		iss >> word;
-		prefix = word.substr(1);
-	}
+    if (data[0] == ':') {
+        iss >> word;
+        prefix = word.substr(1);
+    }
 
-	if (!(iss >> command)) {
-		return (err_no_cmd);
-	}
+    if (!(iss >> command)) {
+        return (false);
+    }
 
-	while (iss >> word) {
-		if (word[0] == ':') {
-			std::string	trailing;
-			getline(iss, trailing);
-			word.erase(0,1);
-			word += trailing;
-			params.push_back(trailing);
-			break ;
-		}
-		params.push_back(word);
-	}
-	return (0);
+    while (iss >> word) {
+        if (word[0] == ':') {
+            std::string trailing;
+            getline(iss, trailing);
+            word.erase(0, 1);
+            word += trailing;
+            params.push_back(trailing);
+            break;
+        }
+        params.push_back(word);
+    }
+    return (true);
 }
 
-int Message::validate(const std::string& prefix, const std::string& command, const std::vector<std::string>& params) {
-	if (!prefix.empty()) {
-		for (size_t i = 0; i < prefix.length(); ++i) {
-			if (!std::isalnum(prefix[i]) && prefix[i] != '-' && prefix[i] != '.') {
-				return (err_syntax);
-			}
-		}
-	}
+bool Message::validate(const std::string& prefix, const std::string& command,
+                       const std::vector<std::string>& params) {
+    if (!prefix.empty()) {
+        for (size_t i = 0; i < prefix.length(); ++i) {
+            if (!std::isalnum(prefix[i]) && prefix[i] != '-' &&
+                prefix[i] != '.') {
+                return (false);
+            }
+        }
+    }
 
-	if (command.empty()) {
-		return (err_syntax);
-	}
-	if (command.length() > 10) {
-		return (err_syntax);
-	}
-	for (std::string::const_iterator it = command.begin(); it != command.end(); ++it) {
-		if (!std::isupper(*it) && !std::isdigit(*it)) {
-			return (err_syntax);
-		}
-	}
+    if (command.empty()) {
+        return (false);
+    }
+    if (command.length() > 10) {
+        return (false);
+    }
+    for (std::string::const_iterator it = command.begin(); it != command.end();
+         ++it) {
+        if (!std::isupper(*it) && !std::isdigit(*it)) {
+            return (false);
+        }
+    }
 
-	if (params.size() > max_params) {
-		return (err_syntax);
-	}
+    if (params.size() > max_params) {
+        return (false);
+    }
 
-	for (size_t i = 0; i < params.size(); ++i) {
-		if (params[i].empty()) {
-			return (err_syntax);
-		}
-		for (size_t j = 0; j < params[i].length(); ++j) {
-			if (!std::isprint(params[i][j])) {
-				return (err_syntax);
-			}
-		}
-	}
+    for (size_t i = 0; i < params.size(); ++i) {
+        if (params[i].empty()) {
+            return (false);
+        }
+        for (size_t j = 0; j < params[i].length(); ++j) {
+            if (!std::isprint(params[i][j])) {
+                return (false);
+            }
+        }
+    }
 
-	return (0);
+    return (true);
 }
