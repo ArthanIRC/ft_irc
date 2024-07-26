93b9e2351a4eac14e47d06d21d698e572090e292
aurlic <arthur.urlic@gmail.com>
2024-07-26 10:58:55 +0200

    feat: Message class structure

--- include/Message.hpp
+++ include/Message.hpp
@@ -1,16 +1,26 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
+#define ERR_NO_CMD 1001
 
 class Message {
   private:
-    std::string _content;
+    std::string               _rawMsg;
+    std::string               _prefix;
+    std::string               _command;
+    std::vector<std::string>  _params;
 
-    void validate(std::string& data);
+    static int parse(std::string& data);
+    static int validate(std::string& data);
 
   public:
-    Message(std::string& data);
     virtual ~Message();
+
+    std::string getRawMsg();
 
     virtual void run() = 0;
 };
+
+static void create(std::string& data);
