2a880f6a58ec15e04295be5117ec92154bfab77c
aurlic <arthur.urlic@gmail.com>
2024-07-26 15:00:35 +0200

    feat: new defines, definite prototypes for parse and validate

--- include/Message.hpp
+++ include/Message.hpp
@@ -5,15 +5,16 @@
 
 class Message {
 	protected:
-		static const unsigned int err_no_cmd = 1001;
+		static const unsigned int	err_no_cmd = 1001;
+		static const unsigned int	err_syntax = 1002;
+		static const unsigned int	max_params = 15;
 
-		std::string					_rawMsg;
 		std::string					_prefix;
 		std::string					_command;
 		std::vector<std::string> 	_params;
 
-		static int parse(std::string& data, std::string& prefix, std::string& command, std::vector<std::string>& params);
-		static int validate(std::string& data);
+		static int parse(std::string& data,std::string& prefix, std::string& command, std::vector<std::string>& params);
+		static int validate(const std::string& prefix, const std::string& command, const std::vector<std::string>& params);
 
 	public:
 		virtual ~Message();
