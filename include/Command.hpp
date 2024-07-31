fc01b55aff72f1c00980deb981f22e6dc52f2d14
aurlic <arthur.urlic@gmail.com>
2024-07-31 10:31:19 +0200

    feat: first work done on command

--- include/Command.hpp
+++ include/Command.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
-class Command {};
+#include "Message.hpp"
+
+class Command : public Message {
+  public:
+    static Command* create(std::string& data);
+
+    virtual void run() = 0; // a modifier jsp encore quoi mettre dedans
+};
