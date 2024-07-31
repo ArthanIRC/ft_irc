7f1df72f14e417bb390d6118c776ac82abc48d6d
aurlic <arthur.urlic@gmail.com>
2024-07-31 10:40:59 +0200

    fix: changed comment

--- include/Command.hpp
+++ include/Command.hpp
@@ -6,5 +6,5 @@ class Command : public Message {
   public:
     static Command* create(std::string& data);
 
-    virtual void run() = 0; // a modifier jsp encore quoi mettre dedans
+    virtual void run() = 0; // a modifier avec un client un channel
 };
