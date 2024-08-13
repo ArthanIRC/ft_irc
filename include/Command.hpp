dbee01df7c992e48e5a75b3f62fe65e4f07b18d0
aurlic <arthur.urlic@gmail.com>
2024-08-13 12:52:51 +0200

    feat: now calling constructors for commands

--- include/Command.hpp
+++ include/Command.hpp
@@ -4,6 +4,8 @@
 
 class Command : public Message {
   public:
+    virtual ~Command(){};
+
     static Command* create(std::string& data);
 
     virtual void run() = 0; // a modifier avec un client un channel
