c7799dd4e7f0a70f32dcc18c7ca03edddd7b970e
Arthur Urlic <arluc@Arthurs-Laptop.local>
2024-08-15 19:51:58 +0200

    fix: new tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "Command.hpp"
 #include <cstdlib>
+#include <iostream>
 
 int main(void) {
-    std::string str = "PASS secret_password\n";
+    std::string str = "INV user #channel\r\n";
 
     Command::create(str);
     return (EXIT_SUCCESS);
