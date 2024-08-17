e80b40c80eb32d0848a22b718e477d85488416f0
Arthur Urlic <arluc@Arthurs-Laptop.local>
2024-08-17 10:17:54 +0200

    refactor: useles attribute

--- include/JoinCommand.hpp
+++ include/JoinCommand.hpp
@@ -3,9 +3,6 @@
 #include "Command.hpp"
 
 class JoinCommand : public Command {
-  private:
-    std::string _channel;
-
   public:
     JoinCommand(std::string prefix, std::vector<std::string> params);
     ~JoinCommand();
