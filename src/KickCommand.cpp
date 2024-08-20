b65dfbec8b63532190e94a43d8998d6a786573f1
Arthur Urlic <arluc@Arthurs-Laptop.local>
2024-08-20 16:46:36 +0200

    not finished

--- src/KickCommand.cpp
+++ src/KickCommand.cpp
@@ -1,23 +1,21 @@
 #include "KickCommand.hpp"
 
-KickCommand::KickCommand(std::string prefix, std::vector<std::string> params) {
-    if (params.size() < 2) {
-        throw std::invalid_argument(
-            "KICK command requires at least two parameters: <channel> <user>.");
-    }
+// KickCommand::KickCommand(std::string prefix, std::vector<std::string> params)
+// {
+// PREFIX pas obligatoire mais a check
+//     if (params.size() < 2) {
+//         throw std::invalid_argument(
+//             "KICK command requires at least two parameters: <channel>
+//             <user>.");
+//     }
+//     std::string channel = params[0];
+//     std::string user = params[1];
+//     std::string comment = params.size() > 2 ? params[2] : "";
 
-    // At this point, you would typically process the command, e.g.:
-    std::string channel = params[0];
-    std::string user = params[1];
-    std::string comment = params.size() > 2 ? params[2] : "";
-
-    // Debug output to verify that the command was parsed correctly
-    std::cout << "KICK command received: Channel = " << channel
-              << ", User = " << user;
-    if (!comment.empty()) {
-        std::cout << ", Comment = " << comment;
-    }
-    std::cout << std::endl;
-
-    // You can add more logic here, like actually processing the kick
-}
+//     std::cout << "KICK command received: Channel = " << channel
+//               << ", User = " << user;
+//     if (!comment.empty()) {
+//         std::cout << ", Comment = " << comment;
+//     }
+//     std::cout << std::endl;
+// }
