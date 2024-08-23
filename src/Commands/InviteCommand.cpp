22c4d09c3af032f3d7851f231bcd3194e889091e
Arthur Urlic <arluc@Arthurs-Laptop.lan>
2024-08-23 15:47:30 +0200

    feat: parsing done

--- src/Commands/InviteCommand.cpp
+++ src/Commands/InviteCommand.cpp
@@ -1,27 +1,32 @@
 #include "InviteCommand.hpp"
 #include "Channel.hpp"
+#include "Client.hpp"
+#include "Replies.hpp"
 #include <exception>
 
 InviteCommand::InviteCommand(std::string source,
                              std::vector<std::string> params, Client* client) {
     Channel* chan;
     try {
-        chan = Server::getInstance().findChannel(params[0]);
+        chan = Server::getInstance().findChannel(params[1]);
     } catch (const std::exception& e) {
         client->sendMessage(Replies::ERR_NOSUCHCHANNEL());
+        throw();
     }
-    try {
-        chan->;
-    } catch (const std::exception& e) {
+    if (!chan->isInChannel(*client)) {
         client->sendMessage(Replies::ERR_NOTONCHANNEL());
+        throw();
+    }
+    if (chan->getInviteOnly() && !chan->isOperator(*client)) {
+        client->sendMessage(Replies::ERR_CHANOPRIVSNEEDED());
+        throw();
+    }
+    if (chan->isInChannel(params[0])) {
+        client->sendMessage(Replies::ERR_USERONCHANNEL());
+        throw();
     }
 }
 
 InviteCommand::~InviteCommand(){};
 
 void InviteCommand::run() { return; }
-
-/*
-Only members of the channel are allowed to invite other users. Otherwise, the
-server MUST reject the command with the ERR_NOTONCHANNEL numeric.
-*/
