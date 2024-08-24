ad13f7edcbefd9bd629704a872e932040d5caea1
traccurt <traccurt@student.42.fr>
2024-08-24 17:19:46 +0200

    feat: parsing done

--- include/Commands/KickCommand.hpp
+++ include/Commands/KickCommand.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "Command.hpp"
+#include <vector>
 
 class KickCommand : public Command {
   private:
-    // std::str
+    Channel* _channel;
+    void checkParams(Client* client, std::vector<std::string> params);
+
   public:
-    KickCommand(std::string prefix, std::vector<std::string> params,
+    KickCommand(std::string source, std::vector<std::string> params,
                 Client* client);
     ~KickCommand();
 
