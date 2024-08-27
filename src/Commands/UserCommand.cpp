078c392e8d2be0323b68d0c7b49d6584c498a59b
Andrew Attali <me@mortebrume.eu>
2024-08-27 14:50:49 +0200

    fix: remove unecessary parsing

--- src/Commands/UserCommand.cpp
+++ src/Commands/UserCommand.cpp
@@ -1,6 +1,5 @@
 #include "UserCommand.hpp"
 #include "Client.hpp"
-#include <stdexcept>
 
 UserCommand::UserCommand(std::string source, std::vector<std::string> params,
                          Client* client) {
@@ -9,20 +8,11 @@ UserCommand::UserCommand(std::string source, std::vector<std::string> params,
         throw;
     }
 
-    try {
-        if (params[3][0] == ':')
-            this->_realname = params[3].substr(1);
-        else
-            this->_realname = params[3];
-    } catch (std::out_of_range const&) {
-        client->sendMessage(Replies::ERR_NEEDMOREPARAMS(client, "USER"));
-        throw;
-    }
-
     this->_source = source;
     this->_params = params;
     this->_client = client;
     this->_username = params[0];
+    this->_realname = params[3];
 }
 
 UserCommand::~UserCommand() {}
