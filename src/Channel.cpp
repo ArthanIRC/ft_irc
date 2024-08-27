6dbe89e78d47a8a98d91690efa46c4a887870967
aurlic <arthur.urlic@gmail.com>
2024-08-27 19:29:34 +0200

    feat: getPrefix done, need to add Voiced map

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -29,6 +29,17 @@ void Channel::checkNameSyntaxChan(std::string& name) {
         throw wrongSyntaxChannelName();
 }
 
+std::string Channel::getPrefix(Client& client) {
+    std::string prefix = "+";
+    if (this->isOperator(client))
+        prefix += "o";
+    if (this->isVoiced(client))
+        prefix += "v";
+    if (prefix == "+")
+        prefix = "";
+    return prefix;
+}
+
 std::string const& Channel::getName() const { return this->_name; }
 
 std::string Channel::getKey() const { return this->_key; }
