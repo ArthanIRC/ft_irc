cf0fdb86fdf16fd44cd2e7023f5ada084c3b40c0
traccurt <traccurt@student.42.fr>
2024-09-05 04:10:07 +0200

    feat: return condition for WhoCommand

--- include/Commands/WhoCommand.hpp
+++ include/Commands/WhoCommand.hpp
@@ -4,11 +4,6 @@
 #include <vector>
 
 class WhoCommand : public Command {
-  private:
-    std::vector<Channel*> _channels;
-    std::string _mask;
-    void checkParams(Client* client, std::vector<std::string> params);
-
   public:
     WhoCommand(std::string source, std::vector<std::string> params,
                Client* client);
--- src/Commands/WhoCommand.cpp
+++ src/Commands/WhoCommand.cpp
@@ -1,9 +1,34 @@
 #include "WhoCommand.hpp"
+#include "Channel.hpp"
+#include "Server.hpp"
+#include <cstddef>
+#include <vector>
 
 WhoCommand::WhoCommand(std::string source, std::vector<std::string> params,
                        Client* client) {
-    checkParams(client, params);
+
     this->_source = source;
     this->_params = params;
     this->_client = client;
+}
+
+void WhoCommand::run() {
+    if (_params.size() < 1)
+        return;
+    Channel* chan;
+    Client* target;
+    try {
+        chan = Server::getInstance().findChannel(_params[0]);
+    } catch (const Server::ChannelNotFoundException&) {
+        chan = NULL;
+    }
+    try {
+        target = Server::getInstance().findClient(_params[0]);
+    } catch (const Server::ClientNotFoundException&) {
+        target = NULL;
+    }
+    if (chan == NULL && target == NULL)
+        return;
+    if (chan != NULL) {
+    }
 }
