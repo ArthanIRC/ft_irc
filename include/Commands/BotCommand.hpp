323b1f9d337104fd2c21ee00c1d57383eaddc591
aurlic <arthur.urlic@gmail.com>
2024-09-15 21:47:11 +0200

    feat: logic for joke and imdad response

--- include/Commands/BotCommand.hpp
+++ include/Commands/BotCommand.hpp
@@ -5,11 +5,19 @@
 class BotCommand : public Command {
   private:
     std::string _key;
+    std::string _target;
+    std::string _sender;
+    std::string _source;
 
   public:
     BotCommand(std::string source, std::vector<std::string> params,
                Client* client);
     ~BotCommand();
 
+    void imDad(std::string name);
+    void joke();
+    std::string joinMessage(std::vector<std::string>& splitData);
+    void checkSender();
+    void dadParse(std::string& data);
     void run();
 };
--- src/Commands/BotCommand.cpp
+++ src/Commands/BotCommand.cpp
@@ -2,6 +2,7 @@
 #include "Client.hpp"
 #include "Exception.hpp"
 #include "JoinCommand.hpp"
+#include <algorithm>
 #include <vector>
 
 using std::map;
@@ -26,6 +27,75 @@ BotCommand::BotCommand(string source, vector<string> params, Client* client) {
 }
 
 BotCommand::~BotCommand() {}
+
+// not sure about function placement etc
+void BotCommand::imDad(string name) {
+    if (_sender[0] == '#') { // check for segfault
+        // send to chan
+    } else {
+        // send to self
+    }
+    std::cout << "Hi " + name + ", I'm Dad !" << std::endl; // tmp test
+}
+
+void BotCommand::joke() {
+    if (_sender[0] == '#') { // check for segfault
+        // send to chan
+    } else {
+        // send to self
+    }
+    std::cout << "You don’t need a parachute to go skydiving. You do need one "
+                 "if you want to go skydiving twice."
+              << std::endl; // tmp test
+    // logic to get a joke from input file
+}
+
+// joins everything after target
+string BotCommand::joinMessage(vector<string>& splitData) {
+    std::ostringstream messageStream;
+
+    for (std::size_t i = 3; i < splitData.size(); ++i) {
+        if (i != 3)
+            messageStream << " ";
+        messageStream << splitData[i];
+    }
+    return messageStream.str();
+}
+
+void BotCommand::checkSender() {
+    _sender = _source.substr(_source.find(":"), _source.find("!"));
+}
+
+void BotCommand::dadParse(string& data) {
+    vector<string> splitData = split(data, ' ');
+    _source = splitData.at(0);
+    _target = splitData.at(2);
+
+    if (splitData.size() >= 4) {
+        std::string fullMessage = BotCommand::joinMessage(splitData);
+        splitData.resize(4);
+        splitData[3] = fullMessage;
+    }
+    checkSender();
+    if (splitData.at(1) == "PRIVMSG") {
+        string& message = splitData.at(3);
+
+        if (message.find("/joke") == 0) {
+            joke();
+            return;
+        }
+
+        string lowerMessage = message;
+        transform(lowerMessage.begin(), lowerMessage.end(),
+                  lowerMessage.begin(), ::tolower);
+        size_t pos = lowerMessage.find("i'm");
+
+        if (pos != string::npos && (pos + 4) < message.size()) {
+            string rest = message.substr(pos + 4);
+            imDad(rest);
+        }
+    }
+}
 
 void BotCommand::run() {
     if (Server::getInstance().getBotKey() != _key) {
